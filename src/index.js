9bddd403bc5f1ee92130b8532a07c1d78a5d60b0
Victor-Mannelli <imp.mannelli@gmail.com>
2022-11-23 15:06:23 -0300

    fix: port

--- src/index.js
+++ src/index.js
@@ -11,6 +11,6 @@ app.use(cors());
 app.use(express.json());
 app.use(indexRouter);
 
-const port = 5000 || process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
