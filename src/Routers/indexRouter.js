a7d1fb19d11d62de55c73248b5c236ea5ebfd65c
Victor-Mannelli <imp.mannelli@gmail.com>
2022-11-24 15:26:43 -0300

    feat: products list router and controller

--- src/Routers/indexRouter.js
+++ src/Routers/indexRouter.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authRouter from "./authRouter.js";
+import productsRouter from "./productsRouter.js";
 
 const indexRouter = Router();
 
 indexRouter.use(authRouter);
+indexRouter.use(productsRouter);
 
 export default indexRouter;
